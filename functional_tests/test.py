18e701243fb40a8214aa868f38f8a4075a7fa971
Jason Lai <jason.lai@taskaprosthetics.com>
2021-03-15 13:51:45 +1300

    new URL, view and template to display lists

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -94,8 +94,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assergNotIn('Buy peacock feathers', page_text)
-        self.assergNotIn('make a fly', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
@@ -112,7 +112,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_elements_by_tag_name('body').text
-        self.assergNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
