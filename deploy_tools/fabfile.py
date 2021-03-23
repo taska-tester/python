cfb9c6082068f75c44059f7bbb9f6b7b4914f0f6
Jason Lai <jason.lai@taskaprosthetics.com>
2021-03-23 13:50:48 +1300

    update fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -16,7 +16,7 @@ def deploy():
 
 def _get_latest_source():
 	if exists('.git'):
-			run ('git fetch')
+		run ('git fetch')
 	else:
 		run(f'git clone {REPO_URL} .')
 	current_commit = local("git log -n 1 --format=%H", capture=True)
@@ -24,13 +24,13 @@ def _get_latest_source():
 
 def _update_virtualenv():
 	if not exists('virtualenv/bin/pip'):
-		run(f'python3.7 -m venv virtualenv)
+		run(f'python3.7 -m venv virtualenv')
 	run('./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
 	append('.env', 'DJANGO_DEBUG_FALSE=y')
 	append('.env', f'SITENAME={env.host}')
-	current_contents = run('cat.env')
+	current_contents = run('cat .env')
 	if 'DJANGO_SECRET_KEY' not in current_contents:
 		new_secret = ''.join(random.SystemRandom().choices(
 			'abcdefghijklmnopqrstuvwxyz0123456789', k=50
