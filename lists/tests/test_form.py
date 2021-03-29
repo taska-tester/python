74035e7d8c8380007f3674a4d18a183213e1b3e6
Jason Lai <jason.lai@taskaprosthetics.com>
2021-03-29 14:26:30 +1300

    rename all item input ids and names. still broken

--- lists/tests/test_form.py
+++ lists/tests/test_form.py
@@ -3,10 +3,6 @@ from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
 
-    # def test_form_renders_item_text_input(self):
-    #     form = ItemForm()
-    #     self.fail(form.as_p())
-
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
@@ -19,4 +15,3 @@ class ItemFormTest(TestCase):
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
-        # form.save()
--- lists/views.py
+++ lists/views.py
@@ -10,13 +10,13 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
+        Item.objects.create(text=request.POST['text'], list=list_)
         return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -32,7 +32,7 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
