b495d0ad3cd4c2dac73188ba435e03ea400195a3
Jason Lai <jason.lai@taskaprosthetics.com>
2021-03-31 12:58:26 +1300

    update setting for deploy

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,16 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# if 'DJANGO_DEBUG_FALSE' in os.environ:
-#     DEBUG = False
-#     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-#     ALLOWED_HOSTS = [os.environ['SITENAME']]
-# else:
-#     DEGUB = True
-    # SECRET_KEY = 'insecure-key-for-dev'
-#     ALLOWED_HOSTS = []
-SECRET_KEY = 'insecure-key-for-dev'
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEGUB = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
+
+# local host script
+# SECRET_KEY = 'insecure-key-for-dev'
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
