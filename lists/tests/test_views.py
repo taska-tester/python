f1afdd2f93fc4e61c6475defb8f19bcc58599857
Jason Lai <jason.lai@taskaprosthetics.com>
2021-03-30 14:50:23 +1300

    Using the Existing List Item Form in the List View

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,8 +8,8 @@ from unittest import skip
 from lists.models import Item, List
 from lists.views import home_page
 from lists.forms import (
-    ItemForm, EMPTY_ITEM_ERROR,
-    DUPLICATE_ITEM_ERROR, ExistingListItemForm
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
 )
 class HomePageTest(TestCase):
 
@@ -95,13 +95,12 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -110,16 +109,14 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
         expected_error = escape(DUPLICATE_ITEM_ERROR)
-
-        expected_error=escape("You've already got this in your list")
         self.assertContains(response, expected_error)
-        self.assertTemplateUsed(ressponse, 'list.html')
+        self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 class NewListTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['text'], list=list_)
-        return redirect(f'/lists/{list_.id}/')
-    return render(request, 'list.html', {'list': list_})
+# def view_list(request, list_id):
+#     list_ = List.objects.get(id=list_id)
+#     if request.method == 'POST':
+#         Item.objects.create(text=request.POST['text'], list=list_)
+#         return redirect(f'/lists/{list_.id}/')
+#     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     form = ItemForm(data=request.POST)
@@ -26,10 +26,10 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
+            form.save()
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
