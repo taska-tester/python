571e2257933f154e6bfdef84d88c98915ced0ccd
Jason Lai <jason.lai@taskaprosthetics.com>
2021-04-09 13:46:49 +1200

    correct logging

--- superlists/settings.py
+++ superlists/settings.py
@@ -136,11 +136,16 @@ EMAIL_USE_TLS =True
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False
+    'disable_existing_loggers': False,
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
         },
     },
     'root': {'level': 'INFO'},
