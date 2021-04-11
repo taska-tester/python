21490bcc515ab87d081abcc894256f27489cf1fe
Jason Lai <jason.lai@taskaprosthetics.com>
2021-04-12 09:50:18 +1200

    add management to create session for server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,33 +1,41 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 import time
 import os
 
 MAX_WAIT = 10
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
+
 
     def tearDown(self):
         self.browser.quit()
 
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
+
+    @wait
+    def wait_for(self, fn):
+        return fn()
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
@@ -35,8 +43,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
 
     @wait
     def wait_to_be_logged_in(self, email):
@@ -44,12 +54,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
-    @wait
-    def wait_to_be_logged_out(self, email):
-        lambda: self.browser.find_element_by_name('email')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(email, navbar.text)
 
     @wait
-    def wait_for(self, fn):
-        return fn()
+    def wait_to_be_logged_out(self, email):
+        self.browser.find_element_by_name('email')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
--- superlists/settings.py
+++ superlists/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
-    'accounts'
+    'accounts',
+    'functional_tests',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
